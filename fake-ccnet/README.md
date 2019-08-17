14897177349d79c8c12bdb5e9cba3dcb61872f39
BaptisteNVM <42940675+BaptisteNVM@users.noreply.github.com>
2019-08-17 23:50:48 +0100

    Fake ccnet server to try to poll data instead of reading the xml file from the web app

---
+++ fake-ccnet/README.md
@@ -0,0 +1,1 @@
+A node app serving test.xml on localhost:8282/ccnet/XmlStatusReport used for local testing  
---
+++ fake-ccnet/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const reportPath = path.join(__dirname, "report.xml");
+const fs = require('fs');
+
+const route = "/ccnet/XmlStatusReport";
+app.get(route, (req, res) => {
+    res.set('Content-Type', 'text/xml');
+
+    fs.readFile(reportPath, function(err, report){
+        if(err) throw err;
+        console.log(`[${new Date().toLocaleTimeString()}] Read ${path.basename(reportPath)}`);
+        res.send(report);
+    });
+});
+
+const port = 8282;
+app.listen(port, () => console.log(`report.xml available at http://localhost:${port}${route}`));
